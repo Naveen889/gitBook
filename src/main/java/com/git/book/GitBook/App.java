590427d6e252916a9bb520d6023e41a47ebe72dc
Naveen889 <Naveenreddy15151@gmail.com>
2020-10-31 11:00:01 +0530

    Added gitBook project to local repo

---
+++ src/main/java/com/git/book/GitBook/App.java
@@ -0,0 +1,16 @@
+package com.git.book.GitBook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void Data(String name) {
+		System.out.println("name= "+name);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
