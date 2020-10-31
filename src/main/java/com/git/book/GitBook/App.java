a13ae981815927a94a75cefe2307b44f3a2a6a80
Naveen889 <Naveenreddy15151@gmail.com>
2020-10-31 11:04:55 +0530

    Added if condition to check the data

--- src/main/java/com/git/book/GitBook/App.java
+++ src/main/java/com/git/book/GitBook/App.java
@@ -8,6 +8,9 @@ public class App
 {
 	public void Data(String name) {
 		System.out.println("name= "+name);
+		if(name.equals("javaTechie")) {
+			System.out.println("");
+		}
 	}
     public static void main( String[] args )
     {
